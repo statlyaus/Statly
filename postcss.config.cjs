4c410d8a1d0a6c10b1ed0e3fda44474a4e782a87
statlyaus <robaddisonlbm@gmail.com>
2025-08-02 21:23:32 +0000

    commit

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,9 +1,8 @@
 // postcss.config.cjs
-const tailwindcss = require('@tailwindcss/postcss');
-
 module.exports = {
-  plugins: [
-    tailwindcss,
-    require('autoprefixer'),
-  ],
+  plugins: {
+    'tailwindcss/nesting': require('tailwindcss/nesting'),
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
