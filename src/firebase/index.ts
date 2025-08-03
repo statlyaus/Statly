9da73fc4867f2345c03cbda7bc3b34fb336aa70a
statlyaus <robaddisonlbm@gmail.com>
2025-08-03 22:49:17 +1000

    refactor: move firebase config to directory for alias

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,4 +1,4 @@
-// src/firebase.ts
+// src/firebase/index.ts
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
