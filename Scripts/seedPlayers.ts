7998f6dc09d41ae8c91c201e0314bb57aff60796
statlyaus <robaddisonlbm@gmail.com>
2025-08-04 01:11:04 +1000

    fix: generate static player params

--- Scripts/seedPlayers.ts
+++ Scripts/seedPlayers.ts
@@ -3,7 +3,7 @@ import fs from 'fs/promises';
 import { initializeApp, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import { z } from 'zod';
-import serviceAccountRaw from '../serviceAccountKey.json' assert { type: 'json' };
+import serviceAccountRaw from '../secrets/serviceAccountKey.json' assert { type: 'json' };
 import type { ServiceAccount } from 'firebase-admin/app';
 
 const serviceAccount = serviceAccountRaw as ServiceAccount;
--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -1,6 +1,6 @@
 // lib/firebaseAdmin.ts
 import { initializeApp, cert, getApps, getApp } from 'firebase-admin/app';
-import { getFirestore } from 'firebase-admin/firestore';
+import { getFirestore, Firestore } from 'firebase-admin/firestore';
 
 const firebaseAdminConfig = {
   projectId: process.env.FIREBASE_PROJECT_ID,
@@ -8,18 +8,22 @@ const firebaseAdminConfig = {
   privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
 };
 
+let adminDb: Firestore | undefined;
+
 if (
-  !firebaseAdminConfig.projectId ||
-  !firebaseAdminConfig.clientEmail ||
-  !firebaseAdminConfig.privateKey
+  firebaseAdminConfig.projectId &&
+  firebaseAdminConfig.clientEmail &&
+  firebaseAdminConfig.privateKey
 ) {
-  throw new Error('Missing Firebase Admin environment variables');
+  const app = getApps().length
+    ? getApp()
+    : initializeApp({
+        credential: cert(firebaseAdminConfig),
+      });
+
+  adminDb = getFirestore(app);
+} else {
+  console.warn('Missing Firebase Admin environment variables');
 }
 
-const app = getApps().length
-  ? getApp()
-  : initializeApp({
-      credential: cert(firebaseAdminConfig),
-    });
-
-export const adminDb = getFirestore(app);
+export { adminDb };
