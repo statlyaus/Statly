5213dd852a18c181ca167f6b5f43f6f20647c0f7
statlyaus <robaddisonlbm@gmail.com>
2025-08-05 23:56:37 +0000

    fix: update import path for global styles in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
 import { AuthProvider } from '@/app/Context/AuthContext';
-import '../styles/globals.css';
+import '../globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
