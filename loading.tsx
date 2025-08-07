7ec4451a023bfd5df95658b2797e970a0a45eebe
statlyaus <robaddisonlbm@gmail.com>
2025-08-07 15:18:04 +0000

    feat: add Loading component with spinner for improved user experience

--- loading.tsx
+++
@@ -1,9 +0,0 @@
-import LoadingSpinner from "@/components/LoadingSpinner";
-
-export default function Loading() {
-  return (
-    <div className="flex h-screen w-full items-center justify-center">
-      <LoadingSpinner />
-    </div>
-  );
-}
