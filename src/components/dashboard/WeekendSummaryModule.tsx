d3039ef93ae88c349702f25eb042dddd4f20cdec
statlyaus <robaddisonlbm@gmail.com>
2025-08-13 02:05:57 +0000

    feat: Implement WeekendSummaryModule for displaying weekend summaries

---
+++ src/components/dashboard/WeekendSummaryModule.tsx
@@ -0,0 +1,13 @@
+import WeekendSummary from '../WeekendSummary';
+
+interface WeekendSummaryModuleProps {
+  refreshTrigger: number;
+}
+
+export default function WeekendSummaryModule({ refreshTrigger: _refreshTrigger }: WeekendSummaryModuleProps) {
+  return (
+    <div className="h-full">
+      <WeekendSummary />
+    </div>
+  );
+}
