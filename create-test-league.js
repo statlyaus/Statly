7044121802e4620d34565c92e00108e64041540c
statlyaus <robaddisonlbm@gmail.com>
2025-08-17 18:10:24 +0000

    feat: update Firebase Admin imports to use ES module syntax

--- create-test-league.js
+++ create-test-league.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
+import { initializeApp, cert } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+import serviceAccount from './statly-4cbed-firebase-adminsdk-fbsvc-7df0e3dae3.json' assert { type: 'json' };
+
 // Initialize Firebase Admin
 const serviceAccount = require('./statly-4cbed-firebase-adminsdk-fbsvc-7df0e3dae3.json');
 initializeApp({
