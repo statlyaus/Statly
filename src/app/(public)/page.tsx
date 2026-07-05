e0e8021d089eae5bd42dd20d40e2a523b9dcdd52
statlyaus <robaddisonlbm@gmail.com>
2026-07-05 11:34:48 +1000

    Trace crisp hero logo

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -175,7 +175,7 @@ export default function HomePage(): ReactElement {
             <h1 className="relative">
               <span className="sr-only">Statly</span>
               <Image
-                src="/brand/statly-primary-logo-hero-sharp.png"
+                src="/brand/statly-primary-logo-hero-crisp.svg"
                 alt=""
                 width={624}
                 height={236}
